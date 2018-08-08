3484700a9abc4b697c8b72aef911edb5b2a886a1
BlondeVillain <bradley.shadowens@gmail.com>
2018-08-07 21:28:10 -0500

    firstuplad

--- classes.js
+++ classes.js
@@ -28,7 +28,21 @@
 
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
-
+class Employee{
+  constructor(first_name, last_name, email, age)
+  {
+    this.first_name = first_name 
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+  }
+  makeWidget(){
+    return this.first_name + this.last_name + "Widget"
+  }
+}
+var guy = new Employee("Boss", "Man", "bossman@fuckidk.com", 99);
+console.log(guy);
+guy.makeWidget();
 //Code Here
 
 
@@ -48,21 +62,36 @@
 
   Call your new class Manager
 */
-
-//Code Here
-
-
+class Manager{
+  constructor(first_name, last_name, email, age, reports)
+  {
+    this.first_name = first_name 
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports(employee = []) = reports
+  }
+  makeWidget(){
+    return this.first_name + this.last_name + "Widget"}
+  hire(employee){employee.push[employee]}
+  fire(index){employee.splice(index, 1)}
+}
+Bill = new Manager("", "", "", "", reports(""))
+Bill.hire("Billy")
+Bill.fire(0)
 
 ////////// PROBLEM 3 //////////
 
 /*
-  Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
+  Managers for Widget Co. get promoted when they get more employees,
+  and get a bonus when they fire employees.
   Progressive Managers have all the same properties as the manager,
   but they also have the following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
 
-  When employees are added or removed we need to check and update their title. Their titles are as follows:
+  When employees are added or removed we need to check and update their title.
+  Their titles are as follows:
     0 : Not a manager
     1-3 : Barely Manager
     4-10 : Mostly Manager
@@ -74,9 +103,35 @@
 
   Call your new class ProgressiveManager
 */
+class ProgressiveManager{
+  constructor(first_name, last_name, email, age, reports, title, bonus)
+  {
+    this.first_name = first_name 
+    this.last_name = last_name
+    this.email = email
+    this.age = age
+    this.reports(employee = []) = reports
+    this.title = title
+    this.bonus = bonus
+  }
+  makeWidget(){
+    return this.first_name + this.last_name + "Widget"}
+  hire(employee){employee.push[employee]}
+  fire(index){employee.splice(index, 1)}
+  title(result)
+  {
+    if (result = null)
+    {return 'Not a manager'}
+  //Program the title update for the prescribed scores above.
+  }
 
-//Code Here
-
+  bonus(employeeFiring)
+  {
+    if (employeeFiring = null)
+    {return 0}
+    //Program $100 bonus for each emplyee fired.
+  }
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
@@ -91,7 +146,15 @@
     - wear_and_tear_count - default 0
     - needs_reboot - default false
 
-  A Machine has the following methods:
+  
+*/
+class Machine{
+  { 
+    this.widgets_made_count = widgets_made_count
+    this.wear_and_tear_count = wear_and_tear_count
+    this.needs_reboot = needs_reboot
+  }
+  /*A Machine has the following methods:
     - makeWidgets
         - This function takes in a number and increases widgets_made_count by that amount
         - It also increases wear_and_tear_count by 1 for every 50
@@ -100,8 +163,18 @@
     - reboot
         - This function returns a function that is called when the machine is done rebooting
         - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
-*/
-
-//Code Here
+  bonus(employeeFiring)*/
+  makeWidgets(number){widgets_made_count + number}
+  fixMachine(){needs_reboot = true}
+  reboot()
+  {
+    return finishrebot()
+    {
+      return "Reboot Complete"
+    }
+    wear_and_tear_count - 10;
+    needs_reboot = false;
+  }
+}
 
 
